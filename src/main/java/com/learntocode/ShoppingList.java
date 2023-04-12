245f9b09a995f50c9834685f9c554f40b4d4134f
MoeSulz <msuleiman@appdev.yearup.org>
2023-04-12 11:30:45 -0400

    Added new shopping list

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1. 2 Cartons of eggs");
+        System.out.println("2. Gallon of milk");
+        System.out.println("3. 6 ounces of celery");
+        System.out.println("4. Fruity Pebbles cereal");
+        System.out.println("5. 2 packs of American cheese");
+        System.out.println("6. Toilet paper");
+        System.out.println("7. Pack of slice bread");
+        System.out.println("8. Cream cheese");
+        System.out.println("9. Bounty paper towels");
+        System.out.println("10. Doritos chips");
+    }
+}
